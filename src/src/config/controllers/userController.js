f4fec2c27ea124e029fe460a5198b51533d6613a
rajababu <123633963+umarei@users.noreply.github.com>
2025-01-24 08:12:55 +0530

    Remove unused files and configurations from node_modules

--- src/src/config/controllers/userController.js
+++ src/src/config/controllers/userController.js
@@ -5,6 +5,7 @@ const User = require('../models/userModel');
 const getUsers = async (req, res) => {
     try {
         const users = await User.find();
+
         res.json(users);
     } catch (error) {
         res.status(500).json({ message: 'Server error' });
